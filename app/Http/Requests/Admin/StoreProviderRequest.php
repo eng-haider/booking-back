f3b84132093f076f3e1b0ba00e688eacce6660db
eng-haider <31736386+eng-haider@users.noreply.github.com>
2025-12-24 12:05:03 +0300

    s
    s

--- app/Http/Requests/Admin/StoreProviderRequest.php
+++ app/Http/Requests/Admin/StoreProviderRequest.php
@@ -11,7 +11,7 @@ class StoreProviderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user()?->role === 'admin';
+        return true;
     }
 
     /**
